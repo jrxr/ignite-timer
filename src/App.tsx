a74094d6233c4bbf38f9ba87fe3179107cf60a0e
Agacy junior <juniormartinssom@gmail.com>
2022-07-25 08:44:02 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
@@ -9,7 +11,8 @@ export function App() {
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
-      <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
